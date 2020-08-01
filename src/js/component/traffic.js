0538078195fae8b8c31f1505d1873b90af2abc68
ALICARMONA <afacm94@gmail.com>
2020-08-01 19:16:43 +0000

    sin funcionalidad

---
+++ src/js/component/traffic.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function Traffic() {
+	return (
+		<div>
+			<div id="trafficTop" />
+			<div id="container">
+				<div className="red light" />
+				<div className="yellow light" />
+				<div className="green light" />
+			</div>
+		</div>
+	);
+}

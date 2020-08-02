61eb10bda210154253eb3c44925e9f1e33426001
ALICARMONA <afacm94@gmail.com>
2020-08-02 00:10:26 +0000

    version final

--- src/js/component/traffic.js
+++ src/js/component/traffic.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+export function Traffic(props) {
+	const [state, setState] = useState({ clickedLight: null });
 
-export function Traffic() {
+	let redSelected = "";
+	if (state.clickedLight === "red") redSelected = "selected";
+	let yellowSelected = "";
+	if (state.clickedLight === "yellow") yellowSelected = "selected";
+	let greenSelected = "";
+	if (state.clickedLight === "green") greenSelected = "selected";
+
 	return (
-		<div>
-			<div id="trafficTop" />
-			<div id="container">
-				<div className="red light" />
-				<div className="yellow light" />
-				<div className="green light" />
+		<>
+			<div id="trafficTop">
+				<div id="container">
+					<div
+						className={"red light " + redSelected}
+						onClick={() => setState({ clickedLight: "red" })}
+					/>
+					<div
+						className={"yellow light" + yellowSelected}
+						onClick={() => setState({ clickedLight: "yellow" })}
+					/>
+					<div
+						className={"green light" + greenSelected}
+						onClick={() => setState({ clickedLight: "green" })}
+					/>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
